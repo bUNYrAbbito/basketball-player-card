7b85a5582e452d44447a1f0961f4ceecdbcd48b0
bUNYrAbbito <singhbunny116@gmail.com>
2024-07-12 11:36:15 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import BasketballPlayerCard from './BasketballPlayerCard';
 
 function App() {
+  const player = {
+    name: "PrasHant Singh",
+    image: "https://cdn.pixabay.com/photo/2022/02/23/20/17/man-7031423_1280.png",
+    position: "Forward",
+    stats: {
+      pointsPerGame: 25.4,
+      assistsPerGame: 7.1,
+      reboundsPerGame: 10.5
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BasketballPlayerCard 
+        name={player.name}
+        image={player.image}
+        position={player.position}
+        stats={player.stats}
+      />
     </div>
+    
   );
 }
 
